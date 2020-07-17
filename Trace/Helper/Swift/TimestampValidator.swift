7e09501159c463738de12694960017a92133cd3e
Shams Ahmed <shamsahmed@me.com>
2020-07-17 17:27:49 +0100

    timestamp validator

---
+++ Trace/Helper/Swift/TimestampValidator.swift
@@ -0,0 +1,38 @@
+//
+//  TimestampValidator.swift
+//  Trace
+//
+//  Created by Shams Ahmed on 17/07/2020.
+//  Copyright © 2020 Bitrise. All rights reserved.
+//
+
+import Foundation
+
+internal struct TimestampValidator {
+
+    // MARK: - Property
+    
+    let toDate: Date
+    
+    // MARK: - Validator
+    
+    func isValid(timestamp: Time.Timestamp) -> Bool {
+        return isValid(seconds: timestamp.seconds, nanos: timestamp.nanos)
+    }
+    
+    func isValid(seconds: Int, nanos: Int) -> Bool {
+        let joinedValue = Double("\(seconds).\(nanos)") ?? 0.0
+        let date = Date(timeIntervalSince1970: joinedValue)
+        let components = Calendar.current.dateComponents(
+            [.year, .month, .day],
+            from: date,
+            to: toDate
+        )
+
+        if let year = components.year, let month = components.month, let day = components.day, year == 0, month == 0, day == 0 {
+            return true
+        }
+        
+        return false
+    }
+}
