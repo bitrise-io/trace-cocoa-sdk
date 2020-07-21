97393cb157a9280cf93c9fbde7862303886abf4f
Shams Ahmed <shamsahmed@me.com>
2020-07-21 12:21:38 +0100

    Removed ViewControllerFormatter

--- Trace/View/ViewController+Swizzled.swift
+++ Trace/View/ViewController+Swizzled.swift
@@ -37,30 +37,11 @@ internal extension UIViewController {
     // MARK: - Enum
     
     private struct AssociatedKey {
-        static var session = "br_viewController_session"
         static var trace = "br_viewController_trace"
         static var foreground = "br_viewController_willEnterForegroundNotification"
     }
     
     // MARK: - Property
-    
-    var metric: Metric {
-        get {
-            if let metric = objc_getAssociatedObject(self, &AssociatedKey.session) as? Metric {
-                return metric
-            } else {
-                let metric = Metric()
-                metric.name = "\(type(of: self))"
-                
-                self.metric = metric
-                
-                return metric
-            }
-        }
-        set {
-            objc_setAssociatedObject(self, &AssociatedKey.session, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-        }
-    }
     
     var trace: TraceModel {
         get {
@@ -113,41 +94,17 @@ internal extension UIViewController {
             original: #selector(UIViewController.awakeFromNib),
             alternative: #selector(UIViewController.bitrise_awakeFromNib)
         )
-        let loadView = Selectors(
-            original: #selector(UIViewController.loadView),
-            alternative: #selector(UIViewController.bitrise_loadView)
-        )
         let viewDidLoad = Selectors(
             original: #selector(UIViewController.viewDidLoad),
             alternative: #selector(UIViewController.bitrise_viewDidLoad)
-        )
-        let viewWillAppear = Selectors(
-            original: #selector(UIViewController.viewWillAppear(_:)),
-            alternative: #selector(UIViewController.bitrise_viewWillAppear(_:))
         )
         let viewDidAppear = Selectors(
             original: #selector(UIViewController.viewDidAppear(_:)),
             alternative: #selector(UIViewController.bitrise_viewDidAppear(_:))
         )
-        let viewWillLayoutSubviews = Selectors(
-            original: #selector(UIViewController.viewWillLayoutSubviews),
-            alternative: #selector(UIViewController.bitrise_viewWillLayoutSubviews)
-        )
-        let viewDidLayoutSubviews = Selectors(
-            original: #selector(UIViewController.viewDidLayoutSubviews),
-            alternative: #selector(UIViewController.bitrise_viewDidLayoutSubviews)
-        )
-        let viewWillDisappear = Selectors(
-            original: #selector(UIViewController.viewWillDisappear(_:)),
-            alternative: #selector(UIViewController.bitrise_viewWillDisappear(_:))
-        )
         let viewDidDisappear = Selectors(
             original: #selector(UIViewController.viewDidDisappear(_:)),
             alternative: #selector(UIViewController.bitrise_viewDidDisappear(_:))
-        )
-        let didReceiveMemoryWarning = Selectors(
-            original: #selector(UIViewController.didReceiveMemoryWarning),
-            alternative: #selector(UIViewController.bitrise_didReceiveMemoryWarning)
         )
         let title = Selectors(
             original: #selector(setter: UIViewController.title),
@@ -157,15 +114,9 @@ internal extension UIViewController {
         _ = self.swizzleInstanceMethod(coder)
         _ = self.swizzleInstanceMethod(nibName)
         _ = self.swizzleInstanceMethod(awakeFromNib)
-        _ = self.swizzleInstanceMethod(loadView)
         _ = self.swizzleInstanceMethod(viewDidLoad)
-        _ = self.swizzleInstanceMethod(viewWillAppear)
         _ = self.swizzleInstanceMethod(viewDidAppear)
-        _ = self.swizzleInstanceMethod(viewWillLayoutSubviews)
-        _ = self.swizzleInstanceMethod(viewDidLayoutSubviews)
-        _ = self.swizzleInstanceMethod(viewWillDisappear)
         _ = self.swizzleInstanceMethod(viewDidDisappear)
-        _ = self.swizzleInstanceMethod(didReceiveMemoryWarning)
         _ = self.swizzleInstanceMethod(title)
         
         return .success
@@ -193,8 +144,6 @@ internal extension UIViewController {
         
         // Should i still create Trace object? would it be better to have a condition
         _ = trace
-        
-        metric.initWithCoder = Time.timestamp
     }
 
     @objc
@@ -203,8 +152,6 @@ internal extension UIViewController {
         
         // Should i still create Trace object? would it be better to have a condition
         _ = trace
-        
-        metric.initWithNib = Time.timestamp
     }
     
     @objc
@@ -213,72 +160,27 @@ internal extension UIViewController {
         
         // Should i still create Trace object? would it be better to have a condition
         _ = trace
-        
-        metric.awakeFromNib = Time.timestamp
     }
 
     // MARK: - Load
-    
-    @objc
-    func bitrise_loadView() {
-        self.bitrise_loadView()
-        
-        metric.loadView = Time.timestamp
-    }
 
     @objc
     func bitrise_viewDidLoad() {
         self.bitrise_viewDidLoad()
-        
-        metric.viewDidLoad = Time.timestamp
-        
+                
         observePresentation()
     }
     
     // MARK: - Appear
 
     @objc
-    func bitrise_viewWillAppear(_ animated: Bool) {
-        self.bitrise_viewWillAppear(animated)
-        
-        metric.viewWillAppear = Time.timestamp
-    }
-
-    @objc
-    func bitrise_viewWillLayoutSubviews() {
-        self.bitrise_viewWillLayoutSubviews()
-        
-        metric.viewWillLayoutSubviews = Time.timestamp
-    }
-
-    @objc
-    func bitrise_viewDidLayoutSubviews() {
-        self.bitrise_viewDidLayoutSubviews()
-        
-        metric.viewDidLayoutSubviews = Time.timestamp
-    }
-
-    @objc
     func bitrise_viewDidAppear(_ animated: Bool) {
         observePresentation()
         
         self.bitrise_viewDidAppear(animated)
-        
-        if metric.viewDidAppear == nil {
-            metric.viewDidAppear = Time.timestamp
-            
-            process()
-        }
     }
     
     // MARK: - Disappear
-
-    @objc
-    func bitrise_viewWillDisappear(_ animated: Bool) {
-        self.bitrise_viewWillDisappear(animated)
-        
-        metric.viewWillDisappear = Time.timestamp
-    }
 
     @objc
     func bitrise_viewDidDisappear(_ animated: Bool) {
@@ -289,44 +191,6 @@ internal extension UIViewController {
         removeObservingPresentation()
         
         self.bitrise_viewDidDisappear(animated)
-        
-        if metric.viewDidDisappear == nil {
-            metric.viewDidDisappear = Time.timestamp
-            
-            process()
-        }
-    }
-    
-    // MARK: - Warning
-
-    @objc
-    func bitrise_didReceiveMemoryWarning() {
-        self.bitrise_didReceiveMemoryWarning()
-        
-        if metric.didReceiveMemoryWarning == nil {
-            metric.didReceiveMemoryWarning = Time.timestamp
-            
-            process()
-        }
-    }
-    
-    // MARK: - Process
-    
-    private func process() {
-        guard !isInternalClass else {
-            metric.observed = false
-            
-            return
-        }
-        guard !isBannedClass else {
-            metric.observed = false
-            
-            return
-        }
-        
-        let formatter = ViewControllerFormatter(metric)
-        
-        Trace.shared.queue.add(formatter.metrics)
     }
     
     // MARK: - Observe
