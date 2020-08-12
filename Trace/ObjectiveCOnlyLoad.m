2f8270b075871972ec3dd54bfef01a4faa349902
Shams Ahmed <shamsahmed@me.com>
2020-08-12 16:21:20 +0100

    Validate methods before calling them indirectly

--- Trace/ObjectiveCOnlyLoad.m
+++ Trace/ObjectiveCOnlyLoad.m
@@ -14,13 +14,49 @@
 #import <Trace-Swift.h> // Static library
 #endif
 
+#pragma mark - Constructor/Destructor
+
 /// :nodoc:
 __attribute__((constructor)) static void ObjectiveCOnlyLoad(void) {
-    // The SDK doesn't expose excess methods when developers won't need to use.
+    if (BRTrace.configuration.enabled == NO) {
+        NSLog(@"[Bitrise:Trace/launch] SDK disabled");
+        
+        return;
+    }
+    
+    // Update session timeout values only if the SDK hasn't been started yet
+    if (BRTrace.currentSession != 0) {
+        return;
+    }
+    
+    // The SDK doesn't expose excess methods when developers won't ever need to use.
     // Lucky we have Objective-C runtime to help!
-   
-    // This gets unit tested
-    [[BRTrace class] performSelector:@selector(reset)]; // adds current time
+    if ([[BRTrace class] respondsToSelector:@selector(reset)]) {
+        // This gets unit tested
+        [[BRTrace class] performSelector:@selector(reset)]; // adds current time
+    } else {
+        // Objective-c logger wrapper
+        Class logger = NSClassFromString(@"BRLoggerObjc");
+        
+        if ([logger class]) {
+            if ([[logger class] respondsToSelector:@selector(print:for:)]) {
+                // Avoid unnecessary Xcode warnings
+                #pragma GCC diagnostic ignored "-Wundeclared-selector"
+                #pragma clang diagnostic push
+                #pragma clang diagnostic ignored "-Wundeclared-selector"
+                // Calls print log with the obj-c method signature
+                [logger performSelector:@selector(print:for:)
+                                        withObject:@"Bitrise Trace does not respond to reset() method"
+                                        withObject:@"launch"
+                ];
+                #pragma clang diagnostic pop
+            } else {
+                NSLog(@"[Bitrise:Trace/internalError] BRLoggerObjc print method does not exist");
+            }
+        } else {
+            NSLog(@"[Bitrise:Trace/internalError] BRLoggerObjc class does not exist");
+        }
+    }
     
     // Start SDK
     (void)[BRTrace shared];
@@ -28,15 +64,46 @@ __attribute__((constructor)) static void ObjectiveCOnlyLoad(void) {
 
 /// :nodoc:
 __attribute__((destructor)) static void ObjectiveCOnlyUnload() {
+    if (BRTrace.configuration.enabled == NO) {
+        NSLog(@"[Bitrise:Trace/launch] SDK disabled");
+        
+        return;
+    }
+    
     // Same logs as above
     Class logger = NSClassFromString(@"BRLoggerObjc");
     
-    #pragma GCC diagnostic ignored "-Wundeclared-selector"
-    #pragma clang diagnostic push
-    #pragma clang diagnostic ignored "-Wundeclared-selector"
-    [logger performSelector:@selector(print:for:)
-                            withObject:@"Bitrise Trace unloaded using library destructor"
-                            withObject:@"launch"
-    ];
-    #pragma clang diagnostic pop
+    if ([logger class]) {
+        if ([[logger class] respondsToSelector:@selector(print:for:)]) {
+            #pragma GCC diagnostic ignored "-Wundeclared-selector"
+            #pragma clang diagnostic push
+            #pragma clang diagnostic ignored "-Wundeclared-selector"
+            [logger performSelector:@selector(print:for:)
+                                    withObject:@"Bitrise Trace unloaded using library destructor"
+                                    withObject:@"launch"
+            ];
+            #pragma clang diagnostic pop
+        } else {
+            NSLog(@"[Bitrise:Trace/internalError] BRLoggerObjc print method does not exist");
+        }
+    } else {
+        NSLog(@"[Bitrise:Trace/internalError] BRLoggerObjc class does not exist");
+    }
 }
+
+#pragma mark - ObjectiveCOnlyLoadUsingClassMethod
+
+@interface ObjectiveCOnlyLoadUsingClassMethod : NSObject
+
++ (void)load;
+
+@end
+
+@implementation ObjectiveCOnlyLoadUsingClassMethod
+
++ (void)load {
+    ObjectiveCOnlyLoad();
+}
+
+@end
+    
