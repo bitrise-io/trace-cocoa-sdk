df7fe64c3ca53ffe9ed8d16667a8f00b017f776d
Shams Ahmed <shamsahmed@me.com>
2020-08-21 10:41:49 +0100

    demo view nib view controller

---
+++ iOSDemo/View Controller/NIB/NibViewController.swift
@@ -0,0 +1,38 @@
+//
+//  NibViewController.swift
+//  iOSDemo
+//
+//  Created by Shams Ahmed on 21/08/2020.
+//  Copyright © 2020 Bitrise. All rights reserved.
+//
+
+import UIKit
+
+final class NibView: UIView {
+    
+}
+
+final class NibViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setup()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    // MARK: - Setup
+    
+    private func setup() {
+        
+    }
+}
