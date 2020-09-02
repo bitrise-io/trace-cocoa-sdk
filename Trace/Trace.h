aed65e3a17d0fe79f7aa68f38fc26af08d5bdb8f
Shams Ahmed <shamsahmed@me.com>
2020-09-02 19:21:53 +0100

    Added Trace header and module

---
+++ Trace/Trace.h
@@ -0,0 +1,19 @@
+//
+//  Trace.h
+//  Trace
+//
+//  Created by Shams Ahmed on 02/09/2020.
+//  Copyright © 2020 Bitrise. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Trace.
+FOUNDATION_EXPORT double TraceVersionNumber;
+
+//! Project version string for Trace.
+FOUNDATION_EXPORT const unsigned char TraceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Trace/PublicHeader.h>
+
+#import <Trace/PublicHeader.h>
