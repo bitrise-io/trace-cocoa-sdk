4446e803031f2110972aaa4de3b981a2392113ea
Shams Ahmed <shamsahmed@me.com>
2020-09-03 09:50:11 +0100

    Trace internal build setttings

--- Trace/Trace.h
+++ Trace/Trace.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double TraceVersionNumber;
 FOUNDATION_EXPORT const unsigned char TraceVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Trace/PublicHeader.h>
-
-#import <Trace/PublicHeader.h>
---
+++ TraceInternal/BRMain.h
@@ -0,0 +1,21 @@
+//
+//  BRMain.h
+//  TraceInternal
+//
+//  Created by Shams Ahmed on 02/09/2020.
+//  Copyright © 2020 Bitrise. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// TODO: Used for testing
+@interface BRMain : NSObject
+
+/// TODO: Used for testing
+- (void)test;
+
+@end
+
+NS_ASSUME_NONNULL_END
