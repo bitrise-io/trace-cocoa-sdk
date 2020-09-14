5207d9aa886d21698829597eba4171f6a4b37697
Shams Ahmed <shamsahmed@me.com>
2020-09-14 13:12:55 +0100

    Sync SDK folder

---
+++ SDK/1.6.1/Trace.xcframework/ios-i386_x86_64-simulator/Headers/TraceInternal/TraceInternal.h
@@ -0,0 +1,17 @@
+//
+//  TraceInternal.h
+//  TraceInternal
+//
+//  Created by Shams Ahmed on 02/09/2020.
+//  Copyright © 2020 Bitrise. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Trace.
+FOUNDATION_EXPORT double TraceInternalVersionNumber;
+
+//! Project version string for Trace.
+FOUNDATION_EXPORT const unsigned char TraceInternalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TraceInternal/PublicHeader.h>
