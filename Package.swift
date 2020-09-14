b41a8d920b09d8102f2e8ca9113b628bbb389156
Shams Ahmed <shamsahmed@me.com>
2020-09-14 23:07:24 +0100

    disabled linker settings

--- Package.swift
+++ Package.swift
@@ -11,7 +11,15 @@ let target: Target = .binaryTarget(
     url: "https://github.com/bitrise-io/trace-cocoa-sdk/releases/download/1.6.1/Trace.xcframework.zip",
     checksum: "82f10c051e7f7cb203006f066e80bb9dcab1e0da0771bcdb1622bdf644a62fae"
 )
-// target.linkerSettings = [.linkedLibrary("c++"), .linkedLibrary("z")]
+
+/**
+Adding linkerSettings in Xcode 12 beta causes:
+- Xcode to crash
+- Swift CLI to report exceptions 
+
+TODO: Wait for Xcode 12 GM to be released
+*/
+//target.linkerSettings = [.linkedLibrary("c++"), .linkedLibrary("z")] // Adds libz and libc++ library.
 
 let package = Package(
     name: name,
