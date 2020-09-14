db5f3f29fe8a7792d8a9015290feeed8053d0ae8
Shams Ahmed <shamsahmed@me.com>
2020-09-14 09:50:48 +0100

    fixed package url

--- Package.swift
+++ Package.swift
@@ -15,7 +15,11 @@ let package = Package(
         .library(name: trace, targets: [trace])
     ],
     targets: [
-        .binaryTarget(name: trace, path: "SDK/latest/\(trace).xcframework") // use Github release
+        .binaryTarget(
+            name: trace, 
+            url: "https://github.com/bitrise-io/trace-cocoa-sdk/releases/download/1.6.1/Trace.xcframework.zip", 
+            checksum: "82f10c051e7f7cb203006f066e80bb9dcab1e0da0771bcdb1622bdf644a62fae"
+        )
     ],
     swiftLanguageVersions: [.v5]
 )
