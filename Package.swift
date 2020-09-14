4e52e6cab1b7123a4b2277e979b8c6a0bf53d648
Shams Ahmed <shamsahmed@me.com>
2020-09-14 15:41:22 +0100

    Added linker settings to package swift

--- Package.swift
+++ Package.swift
@@ -2,24 +2,21 @@
 
 import PackageDescription
 
-let packageName = "BitriseTrace"
+let name = "BitriseTrace"
 let trace = "Trace"
+let product: Product = .library(name: trace, targets: [trace])
+
+let target: Target = .binaryTarget(
+    name: trace,
+    url: "https://github.com/bitrise-io/trace-cocoa-sdk/releases/download/1.6.1/Trace.xcframework.zip",
+    checksum: "82f10c051e7f7cb203006f066e80bb9dcab1e0da0771bcdb1622bdf644a62fae"
+)
+target.linkerSettings = [.linkedLibrary("c++"), .linkedLibrary("z")]
 
 let package = Package(
-    name: packageName,
-    platforms: [
-        .iOS(.v10),
-        .macOS(.v10_15)
-    ],
-    products: [
-        .library(name: trace, targets: [trace])
-    ],
-    targets: [
-        .binaryTarget(
-            name: trace, 
-            url: "https://github.com/bitrise-io/trace-cocoa-sdk/releases/download/1.6.1/Trace.xcframework.zip", 
-            checksum: "82f10c051e7f7cb203006f066e80bb9dcab1e0da0771bcdb1622bdf644a62fae"
-        )
-    ],
+    name: name,
+    platforms: [.iOS(.v10), .macOS(.v10_15)],
+    products: [product],
+    targets: [target],
     swiftLanguageVersions: [.v5]
 )
