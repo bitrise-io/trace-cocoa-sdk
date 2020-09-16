784ae39a78bce48919780cea2706ecc5b7371408
Shams Ahmed <shamsahmed@me.com>
2020-09-16 11:54:59 +0100

    Remove swift head validation in favour of ObjC runtime

--- TraceInternal/BRObjectiveCOnlyLoad.m
+++ TraceInternal/BRObjectiveCOnlyLoad.m
@@ -16,43 +16,63 @@
 
 #pragma mark - Constructor/Destructor
 
-#if __has_include(<Trace/Trace-Swift.h>) || __has_include(<Trace-Swift.h>)
 /// Second attempt at starting SDK
 /// Using library constructor to start the SDK
 /// By default when the SDK is moved to memory the SDK `constructor` method is called
+#pragma GCC diagnostic ignored "-Wundeclared-selector"
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wundeclared-selector"
 __attribute__((constructor)) static void BRInternalObjectiveCOnlyLoad(void) {
-    if (BRTrace.configuration.enabled == NO) {
-        NSLog(@"[Bitrise:Trace/launch] SDK disabled");
-
-        return;
-    }
-
-    // Update session timeout values only if the SDK hasn't been started yet
-    if (BRTrace.currentSession != 0) {
-        return;
-    }
-
     // The SDK doesn't expose excess methods when developers won't ever need to use.
     // Lucky we have Objective-C runtime to help!
-    if ([[BRTrace class] respondsToSelector:@selector(reset)]) {
-        // This gets unit tested
-        [[BRTrace class] performSelector:@selector(reset)]; // adds current time
+    Class Trace = NSClassFromString(@"BRTrace");
+    
+    if (Trace) {
+        @try {
+            BOOL isTraceEnabled = [[Trace valueForKeyPath:@"configuration.enabled"] boolValue];
+            
+            if (isTraceEnabled == NO) {
+                NSLog(@"[Bitrise:Trace/launch] SDK disabled");
+
+                return;
+            }
+            
+            NSInteger currentSession = [[Trace valueForKey:@"currentSession"] integerValue];
+
+            // Update session timeout values only if the SDK hasn't been started yet
+            if (currentSession != 0) {
+                if ([Trace respondsToSelector:@selector(shared)]) {
+                    (void)[Trace performSelector:@selector(shared)]; // Start SDK
+                } else {
+                    NSLog(@"[Bitrise:Trace/internalError] Failed to start SDK since BRTrace.shared has not been found");
+                }
+                
+                return;
+            }
+        } @catch (NSException *exception) {
+            NSLog(@"[Bitrise:Trace/internalError] BRTrace property does not exist");
+
+            return;
+        }
+    } else {
+        NSLog(@"[Bitrise:Trace/internalError] BRTrace class does not exist");
+        
+        return;
+    }
+
+    if ([Trace respondsToSelector:@selector(reset)]) {
+        [Trace performSelector:@selector(reset)]; // adds current time
     } else {
         // Objective-c logger wrapper
         Class logger = NSClassFromString(@"BRLoggerObjc");
 
-        if ([logger class]) {
-            // Avoid unnecessary Xcode warnings
-            #pragma GCC diagnostic ignored "-Wundeclared-selector"
-            #pragma clang diagnostic push
-            #pragma clang diagnostic ignored "-Wundeclared-selector"
-            if ([[logger class] respondsToSelector:@selector(print:for:)]) {
+        if (logger) {
+            if ([logger respondsToSelector:@selector(print:for:)]) {
                 // Calls print log with the obj-c method signature
                 [logger performSelector:@selector(print:for:)
                                         withObject:@"Bitrise Trace does not respond to reset() method"
                                         withObject:@"launch"
                 ];
-            #pragma clang diagnostic pop
             } else {
                 NSLog(@"[Bitrise:Trace/internalError] BRLoggerObjc print method does not exist");
             }
@@ -61,31 +81,42 @@ __attribute__((constructor)) static void BRInternalObjectiveCOnlyLoad(void) {
         }
     }
 
-    // Start SDK
-    (void)[BRTrace shared];
+    if ([Trace respondsToSelector:@selector(shared)]) {
+        (void)[Trace performSelector:@selector(shared)]; // Start SDK
+    } else {
+        NSLog(@"[Bitrise:Trace/internalError] Failed to start SDK since BRTrace.shared has not been found");
+    }
 }
 
 /// :nodoc:
 __attribute__((destructor)) static void BRInternalObjectiveCOnlyUnload() {
-    if (BRTrace.configuration.enabled == NO) {
-        NSLog(@"[Bitrise:Trace/launch] SDK disabled");
+    // The SDK doesn't expose excess methods when developers won't ever need to use.
+    // Lucky we have Objective-C runtime to help!
+    Class Trace = NSClassFromString(@"BRTrace");
+    
+    @try {
+        BOOL isTraceEnabled = [[Trace valueForKeyPath:@"configuration.enabled"] boolValue];
         
+        if (isTraceEnabled == NO) {
+            NSLog(@"[Bitrise:Trace/launch] SDK disabled");
+
+            return;
+        }
+    } @catch (NSException *exception) {
+        NSLog(@"[Bitrise:Trace/internalError] BRTrace.configuration.enabled property does not exist");
+
         return;
     }
     
     // Same logs as above
     Class logger = NSClassFromString(@"BRLoggerObjc");
     
-    if ([logger class]) {
-        if ([[logger class] respondsToSelector:@selector(print:for:)]) {
-            #pragma GCC diagnostic ignored "-Wundeclared-selector"
-            #pragma clang diagnostic push
-            #pragma clang diagnostic ignored "-Wundeclared-selector"
+    if (logger) {
+        if ([logger respondsToSelector:@selector(print:for:)]) {
             [logger performSelector:@selector(print:for:)
                                     withObject:@"Bitrise Trace unloaded using library destructor"
                                     withObject:@"launch"
             ];
-            #pragma clang diagnostic pop
         } else {
             NSLog(@"[Bitrise:Trace/internalError] BRLoggerObjc print method does not exist");
         }
@@ -93,8 +124,9 @@ __attribute__((destructor)) static void BRInternalObjectiveCOnlyUnload() {
         NSLog(@"[Bitrise:Trace/internalError] BRLoggerObjc class does not exist");
     }
 }
+#pragma clang diagnostic pop
 
-#pragma mark - ObjectiveCOnlyLoadUsingClassMethod
+#pragma mark - BRInternalObjectiveCOnlyLoadUsingClassMethod
 
 /// First attempt at starting SDK
 /// This class gets called first when the SDK moved into memory as the runtime
@@ -113,4 +145,3 @@ __attribute__((destructor)) static void BRInternalObjectiveCOnlyUnload() {
 }
 
 @end
-#endif
