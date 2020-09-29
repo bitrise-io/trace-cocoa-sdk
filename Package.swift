d48a2230abf0e7f5b9e345f262d62c7a25293a64
Shams Ahmed <shamsahmed@me.com>
2020-09-29 23:07:45 +0100

    Try package from source

--- Package.swift
+++ Package.swift
@@ -1,30 +1,51 @@
 // swift-tools-version:5.3
-
 import PackageDescription
 
 let version = "1.7.1"
 let name = "BitriseTrace"
 let trace = "Trace"
+let traceInternal = "TraceInternal"
 let product: Product = .library(name: trace, targets: [trace])
-let path = "SDK/\(version)/Trace.xcframework"
-
-let target: Target = .binaryTarget(
-    name: trace,
-    path: path
-)
-/**
-Adding linkerSettings in Xcode 12.2 beta causes:
-- Xcode crashes
-- Swift CLI reports exceptions
-
-TODO: Wait for Xcode 12 GM to be released
-*/
-//target.linkerSettings = [.linkedLibrary("c++"), .linkedLibrary("z")] // Adds libz and libc++ library.
+let targets: [Target] = [
+    .target(
+        name: trace,
+        dependencies: [.target(name: traceInternal)],
+        path: "\(trace)/",
+        exclude: []
+    ),
+    .target(
+        name: traceInternal,
+        path: "\(traceInternal)/",
+        exclude: [],
+        publicHeadersPath: ".",
+        cxxSettings: [
+            .define("GCC_ENABLE_CPP_EXCEPTIONS", to: "YES"),
+            .headerSearchPath(".."),
+            .headerSearchPath("../"),
+            .headerSearchPath("CrashReporting/llvm/Support/"),
+            .headerSearchPath("CrashReporting/llvm/Config/"),
+            .headerSearchPath("CrashReporting/llvm/ADT/"),
+            .headerSearchPath("CrashReporting/swift/"),
+            .headerSearchPath("CrashReporting/swift/Basic"),
+            .headerSearchPath("CrashReporting/Recording/"),
+            .headerSearchPath("CrashReporting/Recording/Tools"),
+            .headerSearchPath("CrashReporting/Recording/Monitors"),
+            .headerSearchPath("CrashReporting/Reporting/Filters/"),
+            .headerSearchPath("CrashReporting/Reporting/Tools/")
+        ],
+        linkerSettings: [
+            .linkedLibrary("c++"),
+            .linkedLibrary("z"),
+            .linkedFramework("UIKit")
+        ]
+    )
+]
 
 let package = Package(
     name: name,
-    platforms: [.iOS(.v10), .macOS(.v10_15)],
+    platforms: [.iOS(.v14)],
     products: [product],
-    targets: [target],
-    swiftLanguageVersions: [.v5]
+    targets: targets,
+    swiftLanguageVersions: [.v5],
+    cxxLanguageStandard: .gnucxx11
 )
