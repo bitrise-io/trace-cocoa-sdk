618694ae66ff8d982667010efdfb08bb01facfbb
Shams Ahmed <shamsahmed@me.com>
2020-09-30 10:27:30 +0100

    added header

--- TraceInternal/TraceInternal.h
+++ TraceInternal/TraceInternal.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char TraceInternalVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TraceInternal/PublicHeader.h>
 
-#import <TraceInternal/KSCrashInstallation.h>
+#import <KSCrashInstallation.h>
