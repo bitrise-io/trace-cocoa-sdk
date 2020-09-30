1ca7757ffa24a78f2049a202cb5d65650254cf97
Shams Ahmed <shamsahmed@me.com>
2020-09-30 20:10:20 +0100

    use package style from kscrash

--- Package.swift
+++ Package.swift
@@ -1,119 +1,127 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+let version = "1.7.1"
+let name = "BitriseTrace"
+let trace = "Trace"
+let traceInternal = "TraceInternal"
+let product: Product = .library(name: trace, targets: [trace])
+let targets: [Target] = [
+    .target(
+        name: trace,
+        dependencies: [.target(name: traceInternal)],
+        path: "\(trace)/"
+    ),
+    .target(
+        name: traceInternal,
+        dependencies: [
+            "TraceInternal_Recording", 
+            "TraceInternal_Recording_Monitors", 
+            "TraceInternal_Recording_Tools", 
+            "TraceInternal_Reporting_Filters", 
+            "TraceInternal_Reporting_Filters_Tools", 
+            "TraceInternal_Reporting_Tools", 
+            "TraceInternal_Swift"
+        ],
+        path: "TraceInternal/Installations",
+        publicHeadersPath: ".",
+        cxxSettings: [
+            .headerSearchPath("../CrashReporting/Reporting/Filters"),
+            .headerSearchPath("../CrashReporting/Reporting/Tools"),
+            .headerSearchPath("../CrashReporting/Recording"),
+            .headerSearchPath("../CrashReporting/Recording/Monitors"),
+            .headerSearchPath("../CrashReporting/Recording/Tools")
+        ]
+    ),
+    .target(
+        name: "TraceInternal_Recording",
+        path: "TraceInternal/CrashReporting/Recording",
+        exclude: [
+            "Monitors",
+            "Tools"
+        ],
+        publicHeadersPath: ".",
+        cxxSettings: [
+            .headerSearchPath("Tools"),
+            .headerSearchPath("Monitors"),
+            .headerSearchPath("../Reporting/Filters"),
+            .headerSearchPath("../../Installations")
+        ]
+    ),
+    .target(
+        name: "TraceInternal_Recording_Monitors",
+        path: "TraceInternal/CrashReporting/Recording/Monitors",
+        publicHeadersPath: ".",
+        cxxSettings: [
+            .define("GCC_ENABLE_CPP_EXCEPTIONS", to: "YES"),
+            .headerSearchPath(".."),
+            .headerSearchPath("../Tools"),
+            .headerSearchPath("../../Reporting/Filters"),
+            .headerSearchPath("../../../Installations")
+        ]
+    ),
+    .target(
+        name: "TraceInternal_Recording_Tools",
+        path: "TraceInternal/CrashReporting/Recording/Tools",
+        publicHeadersPath: ".",
+        cxxSettings: [
+            .headerSearchPath(".."),
+            .headerSearchPath("../../swift"),
+            .headerSearchPath("../../swift/Basic"),
+            .headerSearchPath("../../llvm/ADT"),
+            .headerSearchPath("../../llvm/Support"),
+            .headerSearchPath("../../llvm/Config")
+        ]
+    ),
+    .target(
+        name: "TraceInternal_Reporting_Filters",
+        path: "TraceInternal/CrashReporting/Reporting/Filters",
+        exclude: [
+            "Tools"
+        ],
+        publicHeadersPath: ".",
+        cxxSettings: [
+            .headerSearchPath("Tools"),
+            .headerSearchPath("../../Recording"),
+            .headerSearchPath("../../Recording/Monitors"),
+            .headerSearchPath("../../Recording/Tools"),
+            .headerSearchPath("../../../Installations")
+        ]
+    ),
+    .target(
+        name: "TraceInternal_Reporting_Filters_Tools",
+        path: "TraceInternal/CrashReporting/Reporting/Filters/Tools",
+        publicHeadersPath: ".",
+        cxxSettings: [
+            .headerSearchPath("../../../Recording/Tools")
+        ]
+    ),
+    .target(
+        name: "TraceInternal_Reporting_Tools",
+        path: "TraceInternal/CrashReporting/Reporting/Tools",
+        publicHeadersPath: ".",
+        cxxSettings: [
+            .headerSearchPath("../../Recording"),
+            .headerSearchPath("../../Recording/Tools")
+        ]
+    ),
+    .target(
+        name: "TraceInternal_Swift",
+        path: "TraceInternal/CrashReporting/swift/Basic",
+        publicHeadersPath: ".",
+        cxxSettings: [
+            .headerSearchPath(".."),
+            .headerSearchPath("../../llvm/ADT"),
+            .headerSearchPath("../../llvm/Config"),
+            .headerSearchPath("../../llvm/Support")
+        ]
+    )
+]
+
 let package = Package(
-    name: "KSCrash",
-    products: [
-        .library(
-            name: "TraceInternal",
-            targets: ["TraceInternal_Installations"]
-        )
-    ],
-    targets: [
-        .target(
-            name: "TraceInternal_Installations",
-            dependencies: [
-                "TraceInternal_Recording", 
-                "TraceInternal_Recording_Monitors", 
-                "TraceInternal_Recording_Tools", 
-                "TraceInternal_Reporting_Filters", 
-                "TraceInternal_Reporting_Filters_Tools", 
-                "TraceInternal_Reporting_Tools", 
-                "TraceInternal_Swift"
-            ],
-            path: "TraceInternal/Installations",
-            publicHeadersPath: ".",
-            cxxSettings: [
-                .headerSearchPath("../CrashReporting/Reporting/Filters"),
-                .headerSearchPath("../CrashReporting/Reporting/Tools"),
-                .headerSearchPath("../CrashReporting/Recording"),
-                .headerSearchPath("../CrashReporting/Recording/Monitors"),
-                .headerSearchPath("../CrashReporting/Recording/Tools")
-            ]
-        ),
-        .target(
-            name: "TraceInternal_Recording",
-            path: "TraceInternal/CrashReporting/Recording",
-            exclude: [
-                "Monitors",
-                "Tools"
-            ],
-            publicHeadersPath: ".",
-            cxxSettings: [
-                .headerSearchPath("Tools"),
-                .headerSearchPath("Monitors"),
-                .headerSearchPath("../Reporting/Filters"),
-                .headerSearchPath("../../Installations")
-            ]
-        ),
-        .target(
-            name: "TraceInternal_Recording_Monitors",
-            path: "TraceInternal/CrashReporting/Recording/Monitors",
-            publicHeadersPath: ".",
-            cxxSettings: [
-                .define("GCC_ENABLE_CPP_EXCEPTIONS", to: "YES"),
-                .headerSearchPath(".."),
-                .headerSearchPath("../Tools"),
-                .headerSearchPath("../../Reporting/Filters"),
-                .headerSearchPath("../../../Installations")
-            ]
-        ),
-        .target(
-            name: "TraceInternal_Recording_Tools",
-            path: "TraceInternal/CrashReporting/Recording/Tools",
-            publicHeadersPath: ".",
-            cxxSettings: [
-                .headerSearchPath(".."),
-                .headerSearchPath("../../swift"),
-                .headerSearchPath("../../swift/Basic"),
-                .headerSearchPath("../../llvm/ADT"),
-                .headerSearchPath("../../llvm/Support"),
-                .headerSearchPath("../../llvm/Config")
-            ]
-        ),
-        .target(
-            name: "TraceInternal_Reporting_Filters",
-            path: "TraceInternal/CrashReporting/Reporting/Filters",
-            exclude: [
-                "Tools"
-            ],
-            publicHeadersPath: ".",
-            cxxSettings: [
-                .headerSearchPath("Tools"),
-                .headerSearchPath("../../Recording"),
-                .headerSearchPath("../../Recording/Monitors"),
-                .headerSearchPath("../../Recording/Tools"),
-                .headerSearchPath("../../../Installations")
-            ]
-        ),
-        .target(
-            name: "TraceInternal_Reporting_Filters_Tools",
-            path: "TraceInternal/CrashReporting/Reporting/Filters/Tools",
-            publicHeadersPath: ".",
-            cxxSettings: [
-                .headerSearchPath("../../../Recording/Tools")
-            ]
-        ),
-        .target(
-            name: "TraceInternal_Reporting_Tools",
-            path: "TraceInternal/CrashReporting/Reporting/Tools",
-            publicHeadersPath: ".",
-            cxxSettings: [
-                .headerSearchPath("../../Recording"),
-                .headerSearchPath("../../Recording/Tools")
-            ]
-        ),
-        .target(
-            name: "TraceInternal_Swift",
-            path: "TraceInternal/CrashReporting/swift/Basic",
-            publicHeadersPath: ".",
-            cxxSettings: [
-                .headerSearchPath(".."),
-                .headerSearchPath("../../llvm/ADT"),
-                .headerSearchPath("../../llvm/Config"),
-                .headerSearchPath("../../llvm/Support")
-            ]
-        )
-    ],
+    name: name,
+    platforms: [.iOS(.v14)],
+    products: [product],
+    targets: targets,
     cxxLanguageStandard: .gnucxx11
 )
