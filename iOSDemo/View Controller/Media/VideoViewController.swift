3f18f236b27b0555ae82c9edf21298b3d48eaed7
Shams Ahmed <shamsahmed@me.com>
2020-10-09 22:21:58 +0100

    play video

--- iOSDemo/View Controller/Media/VideoViewController.swift
+++ iOSDemo/View Controller/Media/VideoViewController.swift
@@ -11,6 +11,11 @@ import AVKit
 
 final class VideoViewController: UIViewController {
     
+    // MARK: - Property
+    
+    var player: AVPlayer!
+    var playerLayer: AVPlayerLayer!
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -23,13 +28,35 @@ final class VideoViewController: UIViewController {
         super.viewWillAppear(animated)
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        playerLayer.frame = view.bounds
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        
+        
+        player.play()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        player.pause()
     }
     
     // MARK: - Setup
     
     private func setup() {
+        let url: URL! = URL(string: "https://raw.githubusercontent.com/bitrise-io/trace-cocoa-sdk/mediaDemo/assets/file_example_MP4_1920_18MG.mp4")
         
+        player = AVPlayer(url: url)
+        player.automaticallyWaitsToMinimizeStalling = false
+        
+        playerLayer = AVPlayerLayer(player: player)
+        
+        view.layer.addSublayer(playerLayer)
     }
 }
