83442802b11cc0309a8e9db99f34c53afa857aa3
Shams Ahmed <shamsahmed@me.com>
2020-10-09 12:04:05 +0100

    media files and demo storyboards

---
+++ iOSDemo/View Controller/Media/AudioViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AudioViewController.swift
+//  iOSDemo
+//
+//  Created by Shams Ahmed on 09/10/2020.
+//  Copyright © 2020 Bitrise. All rights reserved.
+//
+
+import UIKit
+import AVKit
+
+final class AudioViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setup()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    // MARK: - Setup
+    
+    private func setup() {
+//        let url = URL(string: "")
+//        let session = AVAudioPlayer(contentsOf: url)
+        
+    }
+}
---
+++ iOSDemo/View Controller/Media/VideoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  VideoViewController.swift
+//  iOSDemo
+//
+//  Created by Shams Ahmed on 09/10/2020.
+//  Copyright © 2020 Bitrise. All rights reserved.
+//
+
+import UIKit
+import AVKit
+
+final class VideoViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setup()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    // MARK: - Setup
+    
+    private func setup() {
+        
+    }
+}
