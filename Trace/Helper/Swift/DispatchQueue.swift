1325e2ac001f6ebd3cb4e53019bb4f677ec23ae9
Shams Ahmed <shamsahmed@me.com>
2020-10-13 13:25:17 +0100

    Check for main queue

---
+++ Trace/Helper/Swift/DispatchQueue.swift
@@ -0,0 +1,28 @@
+//
+//  DispatchQueue.swift
+//  Trace
+//
+//  Created by Shams Ahmed on 13/10/2020.
+//  Copyright © 2020 Bitrise. All rights reserved.
+//
+
+import Foundation
+
+extension DispatchQueue {
+    
+    // MARK: - Property
+            
+    static var isMainQueue: Bool {
+        enum Static {
+            static let key: DispatchSpecificKey<Void> = {
+                let key = DispatchSpecificKey<Void>()
+               
+                DispatchQueue.main.setSpecific(key: key, value: ())
+                
+                return key
+            }()
+        }
+        
+        return getSpecific(key: Static.key) != nil
+    }
+}
