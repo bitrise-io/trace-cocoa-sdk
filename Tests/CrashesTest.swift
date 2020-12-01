64221e1ee7d18a0eccdce217cb8c53436905660b
Shams Ahmed <shamsahmed@me.com>
2020-12-01 11:27:03 +0000

    Updated tests to resovle crash model updates

--- Tests/CrashesTest.swift
+++ Tests/CrashesTest.swift
@@ -26,7 +26,22 @@ final class CrashesTests: XCTestCase {
     
     func testCrashes() {
         let report = Data()
-        let crash = Crash(id: "123", timestamp: "2020-03-11 16:35:29.357 +0000", title: "test", report: report)
+        let crash = Crash(
+            id: "123",
+            timestamp: "2020-03-11 16:35:29.357 +0000",
+            title: "test",
+            appVersion: "1.0.0",
+            buildVersion: "1",
+            osVersion: "iOS 13",
+            deviceType: "iPhone",
+            sessionId: "12345-12345",
+            network: "wifi",
+            carrier: "",
+            deviceId: "12345-12345",
+            eventIdentifier:"12345-12345",
+            crashedWithoutSession: false,
+            report: report
+        )
         
         XCTAssertNotNil(crash)
         XCTAssertNotNil(crash.report)
@@ -36,17 +51,47 @@ final class CrashesTests: XCTestCase {
     
     func testCrashes_dictionary() {
         let report = Data()
-        let crash = Crash(id: "123", timestamp: "2020-03-11 16:35:29.357 +0000", title: "test", report: report)
+        let crash = Crash(
+            id: "123",
+            timestamp: "2020-03-11 16:35:29.357 +0000",
+            title: "test",
+            appVersion: "1.0.0",
+            buildVersion: "1",
+            osVersion: "iOS 13",
+            deviceType: "iPhone",
+            sessionId: "12345-12345",
+            network: "wifi",
+            carrier: "",
+            deviceId: "12345-12345",
+            eventIdentifier:"12345-12345",
+            crashedWithoutSession: false,
+            report: report
+        )
         
         let dict = try? crash.dictionary()
         
         XCTAssertNotNil(dict)
-        XCTAssertEqual(dict?.count, 3)
+        XCTAssertEqual(dict?.count, 13)
     }
     
     func testCrashes_json() {
         let report = Data()
-        let crash = Crash(id: "123", timestamp: "2020-03-11 16:35:29.357 +0000", title: "test", report: report)
+        let crash = Crash(
+            id: "123",
+            timestamp: "2020-03-11 16:35:29.357 +0000",
+            title: "test",
+            appVersion: "1.0.0",
+            buildVersion: "1",
+            osVersion: "iOS 13",
+            deviceType: "iPhone",
+            sessionId: "12345-12345",
+            network: "wifi",
+            carrier: "",
+            deviceId: "12345-12345",
+            eventIdentifier:"12345-12345",
+            crashedWithoutSession: false,
+            report: report
+        )
         
         let json = try? crash.json()
         
