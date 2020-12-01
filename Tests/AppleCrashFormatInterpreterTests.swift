1290a67da3c7cbbc3c3810fe27985b8e01da0f14
Shams Ahmed <shamsahmed@me.com>
2020-12-01 15:08:46 +0000

    Added new validation tests

--- Tests/AppleCrashFormatInterpreterTests.swift
+++ Tests/AppleCrashFormatInterpreterTests.swift
@@ -75,6 +75,20 @@ final class AppleCrashFormatInterpreterTests: XCTestCase {
                 XCTAssertNotNil(model.timestamp)
                 XCTAssertFalse(model.id.isEmpty)
                 XCTAssertFalse(model.timestamp.isEmpty)
+                XCTAssertNotNil(model.appVersion)
+                XCTAssertNotNil(model.buildVersion)
+                XCTAssertNotNil(model.osVersion)
+                XCTAssertTrue(!model.deviceType.isEmpty)
+                XCTAssertNotNil(model.sessionId)
+                XCTAssertTrue(!model.eventIdentifier.isEmpty)
+                XCTAssertNotNil(model.network)
+                XCTAssertNotNil(model.carrier)
+                XCTAssertTrue(!model.deviceId.isEmpty)
+                
+                XCTAssertFalse(model.deviceType.contains("\""))
+                XCTAssertFalse(model.network.contains("\""))
+                XCTAssertFalse(model.carrier.contains("\""))
+                XCTAssertFalse(model.deviceId.contains("\""))
             }
         } catch {
             XCTFail("Could not open crash file")
@@ -138,6 +152,26 @@ final class AppleCrashFormatInterpreterTests: XCTestCase {
                 
                 XCTAssertTrue(model.id.isEmpty)
             }
+        } catch {
+            XCTFail("Could not open crash file")
+        }
+    }
+    
+    func testCreateModel_InStringFormat() {
+        guard let url = Bundle(for: Self.self).url(forResource: "traceIdEmpty", withExtension: "crash") else {
+            return XCTFail("Could not locate test fails")
+        }
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let string = String(data: data, encoding: .utf8)!
+            
+            XCTAssertNotNil(string)
+            
+            XCTAssertTrue(string.contains("SDK Event Identifier:"))
+            XCTAssertTrue(string.contains("Build Version:"))
+            XCTAssertTrue(string.contains("App Version:"))
+            XCTAssertTrue(string.contains("app.session.id"))
         } catch {
             XCTFail("Could not open crash file")
         }
