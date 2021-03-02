3184f2c99a04adbddcdcf1b7cbb7ae2400bec35c
Shams Ahmed <shamsahmed@me.com>
2021-03-02 15:05:44 +0000

    Comment for URLSession

---
+++ Tests/URLLocalSessionConnectionTests.swift
@@ -0,0 +1,29 @@
+//
+//  URLLocalSessionConnectionTests.swift
+//  Tests
+//
+//  Created by Shams Ahmed on 26/02/2021.
+//  Copyright © 2021 Bitrise. All rights reserved.
+//
+
+import XCTest
+@testable import Trace
+
+final class URLLocalSessionConnectionTests: XCTestCase {
+    
+    // MARK: - Setup
+    
+    override func setUp() {
+        
+    }
+    
+    override func tearDown() {
+        
+    }
+    
+    // MARK: - Tests
+    
+    func testURLLocalSessionConnectionTests() {
+        
+    }
+}
