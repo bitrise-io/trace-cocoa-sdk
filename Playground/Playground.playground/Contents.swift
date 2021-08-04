651c7a6cc4afc78f2a116ab8d408fceb478dc890
Shams Ahmed <shamsahmed@me.com>
2021-08-04 17:31:59 +0100

    fixed tests

--- Playground/Playground.playground/Contents.swift
+++ Playground/Playground.playground/Contents.swift
@@ -1,3 +1,8 @@
 import UIKit
 
 // This playground acts as a scratchpad for development purposes only.
+let formatter = DateFormatter()
+formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+
+let date = formatter.date(from: "2020-08-14T22:59:59+0000")
+print("1")
