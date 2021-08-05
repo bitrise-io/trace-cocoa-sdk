40df1431f1664fb007c7cf32003cbd7b68600269
Shams Ahmed <shamsahmed@me.com>
2021-08-05 10:28:46 +0100

    removed excess code

--- Playground/Playground.playground/Contents.swift
+++ Playground/Playground.playground/Contents.swift
@@ -1,8 +1,3 @@
 import UIKit
 
 // This playground acts as a scratchpad for development purposes only.
-let formatter = DateFormatter()
-formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-
-let date = formatter.date(from: "2020-08-14T22:59:59+0000")
-print("1")
