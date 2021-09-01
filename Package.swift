5dace831481550cfd1c17d0641b521915c948530
Shams Ahmed <shamsahmed@me.com>
2021-09-01 11:59:21 +0100

    Bumped version to 1.7.39

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.2
 import PackageDescription
 
-let version = "1.7.38"
+let version = "1.7.39"
 let name = "BitriseTrace"
 let trace = "Trace"
 let traceInternal = "TraceInternal"
